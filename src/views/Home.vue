9053b96bbe1c54b4e5e867c6c7c8916d98e46d1e
ZhangBarry <530027054@qq.com>
2020-09-11 15:10:12 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {};
+        },
+        methods: {},
+    }
+</script>
+
+<style scoped lang="scss">
+    .home {
+        img {
+            width: 750px;
+        }
+    }
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,43 @@
+const siteTitle='正尚网络科技'
+
+
+module.exports = {
+    chainWebpack: config => {
+        const oneOfsMap = config.module.rule('scss').oneOfs.store
+        oneOfsMap.forEach(item => {
+            item
+                .use('sass-resources-loader')
+                .loader('sass-resources-loader')
+                .options({
+                    // Provide path to the file with resources
+                    // 要公用的scss的路径
+                    resources: './global.scss'
+                })
+                .end()
+        })
+        config.plugin('html')
+            .tap(args => {
+                args[0].title = siteTitle;
+                return args;
+            })
+    },
+    css: {
+        loaderOptions: {
+            css: {},
+            postcss: {
+                plugins: [
+                    // 补全css前缀(解决兼容性)
+                    require("autoprefixer")(),
+                    // 把px单位换算成rem单位
+                    require("postcss-pxtorem")({
+                        rootValue: 75, // 换算的基数(设计图750的根字体为32)
+                        selectorBlackList: [".van", ".my-van"],// 要忽略的选择器并保留为px。
+                        propList: ["*"], //可以从px更改为rem的属性。
+                        minPixelValue: 2 // 设置要替换的最小像素值。
+                    })
+                ]
+            }
+        }
+    },
+
+}
