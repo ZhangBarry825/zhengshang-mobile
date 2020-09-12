2bfddbc4dc8b0239520ad0a880c5e253a6b588d5
ZhangBarry <530027054@qq.com>
2020-09-12 09:47:32 +0800

    swiper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="home">
+        <div class="swiper-box">
+            <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+                <van-swipe-item>1</van-swipe-item>
+                <van-swipe-item>2</van-swipe-item>
+                <van-swipe-item>3</van-swipe-item>
+                <van-swipe-item>4</van-swipe-item>
+            </van-swipe>
+        </div>
+
     </div>
 </template>
 
@@ -17,8 +26,18 @@
 
 <style scoped lang="scss">
     .home {
-        img {
-            width: 750px;
+        .swiper-box{
+            width: 100%;
+            height: 850px;
+            border: 1px solid red;
+
+            .my-swipe .van-swipe-item {
+                color: #fff;
+                font-size: 20px;
+                line-height: 150px;
+                text-align: center;
+                background-color: #39a9ed;
+            }
         }
     }
 
