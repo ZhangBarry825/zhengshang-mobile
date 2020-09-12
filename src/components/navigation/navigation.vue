3a64d2bc9079206a99404edbdcd4d5a0ee436a74
1433207385@qq.com <yuluo>
2020-09-12 09:40:37 +0800

    0

---
+++ src/components/navigation/navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="navigation">
+    <h2>这是navigation.vue页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+
+  data () {
+    return {
+
+    }
+  },
+  //生命周期函数
+  created () {
+  },
+  //方法
+  methods: {
+  },
+  //组件
+  components: {
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.navigation {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 750px;
+  height: 128px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-    </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <navigation></navigation>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        name: 'Home',
-        components: {},
-        data() {
-            return {};
-        },
-        methods: {},
-    }
+import navigation from '../components/navigation/navigation'
+export default {
+  name: 'Home',
+  components: {},
+  data () {
+    return {};
+  },
+  methods: {},
+  components: {
+    navigation
+  }
+}
 </script>
 
 <style scoped lang="scss">
-    .home {
-        img {
-            width: 750px;
-        }
-    }
-
+.home {
+  img {
+    width: 750px;
+  }
+}
 </style>
